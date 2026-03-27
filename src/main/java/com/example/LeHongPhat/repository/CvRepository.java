610aff012f8559ab2816e9845131d6278bb32ad0
pht <lhp141204@gmail.com>
2026-03-27 20:33:46 +0700

    fix bug

--- src/main/java/com/example/LeHongPhat/repository/CvRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.LeHongPhat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.LeHongPhat.model.Cv;
-
-public interface CvRepository extends JpaRepository<Cv, Long> {
-}
